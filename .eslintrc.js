6e9cf4f6e360e4a70a52f9e89f2181acd14762f5
Alexsandro <alexsandrolima.dev@gmail.com>
2021-08-02 21:46:50 -0300

    Criando Projeto Expo

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+  },
+  extends: 'airbnb',
+  quotes: [2, 'single'],
+};
